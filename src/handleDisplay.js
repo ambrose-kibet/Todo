311cc86c47c99417986167501e7eba81bb9ad45e
ambrose-kibet <ambrosekibet36@gmail.com>
2023-01-26 17:56:39 +0300

    add tests for edit and clear completed items  and Dom manipulation

--- src/handleDisplay.js
+++
@@ -1,9 +0,0 @@
-import displayItems from './displayList';
-import { getLocalStorage } from './LocalStorage';
-
-const handleDisplay = () => {
-  displayItems(getLocalStorage());
-
-  // clear all completed fuctionality
-};
-export default handleDisplay;
