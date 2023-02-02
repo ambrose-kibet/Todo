7bfbcb55c87f7958fe17db196c15b5c0c8bae601
Bigizi <bigizinduwayo@gmail.com>
2023-02-02 14:27:11 +0200

    Add test to clear all button

--- src/todo.test.js
+++ src/todo.test.js
@@ -24,3 +24,68 @@ describe('delete functionality', () => {
     expect(TestFile.listItems.length).toBe(2);
   });
 });
+
+// DOM manipulation
+describe(' displayItems should write files to the dom', () => {
+  it('should return2 ', () => {
+    expect(displayItems(TestFile.listItems)).toBe(2);
+    TestFile.addItem('people');
+    expect(displayItems(TestFile.listItems)).toBe(3);
+    TestFile.deleteItem(0);
+    expect(displayItems(TestFile.listItems)).toBe(2);
+  });
+});
+
+// Edit
+describe('test Edditing fuctionality', () => {
+  it('should return people for item with id 1', () => {
+    TestFile.editItem(1, 'description', 'people');
+    expect(TestFile.listItems.find((i) => i.index === 1)).toEqual({
+      index: 1,
+      description: 'people',
+      completed: false,
+    });
+  });
+  it('should return person for item with id 1', () => {
+    TestFile.editItem(1, 'description', 'person');
+    expect(TestFile.listItems.find((i) => i.index === 1)).toEqual({
+      index: 1,
+      description: 'person',
+      completed: false,
+    });
+  });
+});
+
+// completed status
+describe('test Edditingcompleted status fuctionality', () => {
+  it('should return item with id 1 completed true', () => {
+    TestFile.editItem(1, 'completed');
+    expect(TestFile.listItems.find((i) => i.index === 1)).toEqual({
+      index: 1,
+      description: 'person',
+      completed: true,
+    });
+  });
+  it('should return item with id 1 completed false', () => {
+    TestFile.editItem(1, 'completed');
+    expect(TestFile.listItems.find((i) => i.index === 1)).toEqual({
+      index: 1,
+      description: 'person',
+      completed: false,
+    });
+  });
+});
+
+// clear completed items
+describe('Test clear completed items functionality', () => {
+  it('should have a length of 1', () => {
+    TestFile.editItem(0, 'completed');
+    TestFile.clearCompletedItems();
+    expect(TestFile.listItems).toHaveLength(1);
+  });
+  it('should have a length of 0', () => {
+    TestFile.editItem(0, 'completed');
+    TestFile.clearCompletedItems();
+    expect(TestFile.listItems).toHaveLength(0);
+  });
+});
